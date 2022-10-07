f6a97f8da5b0fc01e8eb6382850dc2defe67f603
george <georgelin@oringnet.com>
2022-10-07 13:23:04 +0800

    add databaseModule to provide db setting for app.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
+import * as Joi from '@hapi/joi';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DataBaseModule } from './database.module';
 
+
+/**
+ * config module creation.
+ * 
+ * Reference:
+ *  - https://wanago.io/2020/05/18/api-nestjs-postgresql-typeorm/
+ */
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        // PORT: Joi.number(),
+      })
+    }),
+    DataBaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database.module.ts
@@ -0,0 +1,32 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+/**
+ * database module creation.
+ * 
+ * Reference:
+ *  - https://wanago.io/2020/05/18/api-nestjs-postgresql-typeorm/
+ */
+@Module({
+    imports: [
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            
+            useFactory: (configService: ConfigService) => ({
+                type: 'postgres',
+                host: configService.get('POSTGRES_HOST'),
+                port: configService.get('POSTGRES_PORT'),
+                username: configService.get('POSTGRES_USER'),
+                password: configService.get('POSTGRES_PASSWORD'),
+                database: configService.get('POSTGRES_DB'),
+                entities: [
+                    __dirname + '/../**/*.entity.ts',
+                ],
+                synchronize: true,
+            })
+        })
+    ],
+})
+export class DataBaseModule {}
